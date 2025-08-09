1bb376dc2d60def8e5484885024269ca6846acfa
FoxMoss <foxmoss@mediaology.com>
2025-08-08 22:17:20 -0500

    fix: no siggy announcements

--- announcement-sender/main.py
+++ announcement-sender/main.py
@@ -14,12 +14,12 @@ import sys
 # 179 bytes: message text, padded with null bytes
 MSG_FMT = "!B32s32sIB179s"
 SIGLESS_MSG_FMT = "!IB179s"
-try:
-    with open("signing_key.txt", "r") as f:
-        sk = PrivateKey.fromString(f.read())
-except FileNotFoundError:
-    print("No signing_key.txt file found. Grab it from Slack.")
-    sys.exit(1)
+# try:
+#     with open("signing_key.txt", "r") as f:
+#         sk = PrivateKey.fromString(f.read())
+# except FileNotFoundError:
+#     print("No signing_key.txt file found. Grab it from Slack.")
+#     sys.exit(1)
 
 
 message = input("Enter announcement message (max 179 characters): ")[:179]
@@ -28,15 +28,14 @@ message_bytes = message_bytes.ljust(179, b"\0")
 timestamp = int(time.time())
 
 sigless_msg = struct.pack(SIGLESS_MSG_FMT, timestamp, len(message), message_bytes)
-signature = Ecdsa.sign(sigless_msg, sk)
-full_msg = struct.pack(MSG_FMT, 0, signature.r.to_bytes(32, byteorder="big"), signature.s.to_bytes(32, byteorder="big"), timestamp, len(message), message_bytes)
+# signature = Ecdsa.sign(sigless_msg, sk)
+full_msg = struct.pack(MSG_FMT, 0, bytes([0] * 32), bytes([0] * 32), timestamp, len(message), message_bytes)
 print(f"Full message length: {len(full_msg)} bytes")
 print(f"Full message: {full_msg}")
 # verify the signature
-vk = sk.publicKey()
-is_valid = Ecdsa.verify(sigless_msg, signature, vk)
-print(f"Signature valid: {is_valid}")
-print(f"Signature: r={signature.r.to_bytes(32, byteorder="big").hex()}, s={signature.s.to_bytes(32, byteorder="big").hex()}")
+# is_valid = Ecdsa.verify(sigless_msg, signature, vk)
+# print(f"Signature valid: {is_valid}")
+# print(f"Signature: r={signature.r.to_bytes(32, byteorder="big").hex()}, s={signature.s.to_bytes(32, byteorder="big").hex()}")
 
 available_ports = serial.tools.list_ports.comports()
 print("Available serial ports:")
