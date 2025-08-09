10ad2d384ed1fba4f73305dd291364bec65ff8d0
BomberFish <bomberfish@bomberfish.ca>
2025-08-09 00:30:09 -0400

    stupid merge

--- Code/apps/home-screen/main.py
+++ Code/apps/home-screen/main.py
@@ -30,7 +30,7 @@ class App(badge.BaseApp):
         badge.utils.set_led_pwm(10000)
 
     def loop(self) -> None:
-        if badge.input.get_button(badge.input.Buttons.SW11):
+        if badge.input.get_button(badge.input.Buttons.SW12):
             self.old_button_b = True
         else:
             if self.old_button_b:
@@ -42,7 +42,7 @@ class App(badge.BaseApp):
                 badge.utils.set_led(False)
             self.old_button_b = False
 
-        if badge.input.get_button(badge.input.Buttons.SW12):
+        if badge.input.get_button(badge.input.Buttons.SW11):
             self.old_button_f = True
         else:
             if self.old_button_f:
