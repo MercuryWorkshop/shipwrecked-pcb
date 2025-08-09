cab912f7804bb2caa13573bf323a048f310c67ec
Toshit Chawda <r58playz@gmail.com>
2025-08-08 22:48:51 -0400

    djihghdsf

--- Code/apps/celeste/main.py
+++ Code/apps/celeste/main.py
@@ -1,12 +1,15 @@
 import badge
-from PICO8 import PICO8
-from Carts.Celeste import Celeste
+import gc
+from apps.celeste.PICO8 import PICO8
+from apps.celeste.Celeste import Celeste
 
 class App(badge.BaseApp):
-    p8 = PICO8(Celeste)
-
     def on_open(self) -> None:
-        self.logger.info("celeste classic 1!!");
+        self.logger.info(f"celeste classic: mem_free {}", gc.mem_free());
+        gc.collect()
+        self.logger.info(f"celeste classic: mem_free after {}", gc.mem_free());
+        self.p8 = PICO8(Celeste)
+        self.logger.info(f"celeste classic: mem_free init {}", gc.mem_free());
 
     def loop(self) -> None:
         l = badge.input.get_button(badge.input.Buttons.SW11);
@@ -15,7 +18,8 @@ class App(badge.BaseApp):
         d = badge.input.get_button(badge.input.Buttons.SW14);
         z = badge.input.get_button(badge.input.Buttons.SW15);
         x = badge.input.get_button(badge.input.Buttons.SW16);
-        p8.set_inputs(l, r, u, d, z, x);
-        p8.step();
+        self.p8.set_inputs(l, r, u, d, z, x);
+        self.p8.step();
 
+        badge.display.fill(1)
         badge.display.nice_text(str(p8.game), 0, 0, font=badge.display.nice_fonts[24], color=0, rot=0, x_spacing=0, y_spacing=0);
